b27f3b058c04ddf39f54eb5c21ff777f4601b577
KevinJoseph <kevramos95@gmail.com>
2022-11-21 00:09:45 -0500

    handlers: implements functions createToken

--- src/handlers/handlerCreateToken.ts
+++ src/handlers/handlerCreateToken.ts
@@ -1,7 +1,12 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { Card } from '../enity/Card';
 import { MongoDbService } from '../services/db/MongoDbService';
+import { validateCreditCard } from '../utils/validateCreditCard';
+import { TokenizationService } from '../services/card/TokenizationService';
+import { Token } from '../enity/Token';
 
 new MongoDbService().connect;
+const tokenizationService = new TokenizationService();
 
 export const createToken = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   let response: APIGatewayProxyResult = {
@@ -10,6 +15,22 @@ export const createToken = async (event: APIGatewayProxyEvent): Promise<APIGatew
   };
 
   try {
+    const requestBody = JSON.parse(event.body as string);
+    await validateCreditCard.validate(requestBody);
+    const card: Card = { ...requestBody };
+    const cardToken: Token = tokenizationService.tokenizeCard(card);
+    const wasCardSaved = await tokenizationService.persist(card, cardToken);
+    if (wasCardSaved) {
+      response.statusCode = 200;
+      response.body = JSON.stringify(cardToken);
+
+      return response;
+    }
+
+    response.body = JSON.stringify({
+      message: 'unable to save card into database, please check the connection',
+    });
+
     response.body = JSON.stringify('ok');
   } catch (error: unknown) {
     response.body = JSON.stringify('error');
