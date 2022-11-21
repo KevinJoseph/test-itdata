bdf05aa4ab0cfba6e3bdd45b7a10717ad223d2f9
KevinJoseph <kevramos95@gmail.com>
2022-11-20 23:21:23 -0500

    fix: add dotenv and configure

--- src/config/db/index.ts
+++ src/config/db/index.ts
@@ -1,10 +1,9 @@
 import { ConfigInterface } from '../ConfigInterface';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 export class MongoDbConfig implements ConfigInterface {
-  mongoUrl: string = '';
+  public mongoUrl: string = '';
 
   load(): void {
     this.mongoUrl = process.env.MONGODB_URL as string;
