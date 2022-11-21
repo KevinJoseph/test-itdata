6ecbdff20bd6c909dcb3e412ceb40cc87e2713e4
KevinJoseph <kevramos95@gmail.com>
2022-11-20 23:57:10 -0500

    validator: add validator mongoose

---
+++ src/validator/CardValidator.ts
@@ -0,0 +1,9 @@
+import { AbstractValidator } from './AbstractValidator';
+
+export class CardValidator extends AbstractValidator {
+  constructor() {
+    super(/^\d{13,16}$/, 'Card number must contains between 13 and 16 numbers');
+  }
+}
+
+export const cardValidator = new CardValidator();
